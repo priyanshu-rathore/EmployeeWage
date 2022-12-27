2b15450389d2f51bb88a82353c7336d14c7df7c7
priyanshu-rathore <priyanshurathore132@gmail.com>
2022-12-27 07:09:37 +0530

    Welcome to Employee wage computation program on master branch

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EmployeeWage{
+    class Program {
+        public static void main(string[] args){
+            System.Console.WriteLine("Welcome to Employee wage computation program on master branch");
+        }
+    }
+}
